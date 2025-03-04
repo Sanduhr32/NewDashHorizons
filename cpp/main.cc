6297747009653ea7452d1e6d9c96d38d9c02ef7e
San32 <25187502+Sanduhr32@users.noreply.github.com>
2025-03-04 13:33:54 +0100

    Move C++ Source to own directory

--- cpp/main.cc
+++ cpp/main.cc
@@ -6,7 +6,7 @@ namespace drogon {
 
 int main() {
     //Load config file
-    drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("config.json");
     //drogon::app().loadConfigFile("../config.yaml");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().registerHandler("/stop", [](const drogon::HttpRequestPtr &req, drogon::Callback &&call) {
