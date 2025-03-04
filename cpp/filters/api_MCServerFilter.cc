507542149ed4976269c2c64f793f9e8caff789bd
San32 <25187502+Sanduhr32@users.noreply.github.com>
2025-03-05 00:44:00 +0100

    manually resolve

--- cpp/filters/api_MCServerFilter.cc
+++ cpp/filters/api_MCServerFilter.cc
@@ -9,40 +9,39 @@
 using namespace drogon;
 using namespace api;
 
+int MCServerFilter::lookup(const std::string& host, int family, std::vector<std::string>& out) {
+    addrinfo hints{AI_CANONNAME, family, SOCK_STREAM}, *res, *result;
+    if (getaddrinfo(host.c_str(), nullptr, &hints, &result))
+    {
+        LOG_ERROR << "could not resolve " << host << " as family " << family;
+        return 1;
+    }
+    res = result;
+    while (res) {
+        if (res->ai_family != family) {
+            res = res->ai_next;
+            continue;
+        }
+        if (res->ai_family == AF_INET) {
+                auto *addr_in = reinterpret_cast<sockaddr_in *>(res->ai_addr);
+                out.emplace_back(inet_ntoa(addr_in->sin_addr));
+        }
+    }
+    freeaddrinfo(result);
+    return 0;
+}
+
 void MCServerFilter::doFilter(const HttpRequestPtr &req,
                          FilterCallback &&fcb,
                          FilterChainCallback &&fccb)
 {
-    auto source = req->peerAddr();
-    auto dnsAddr = std::vector<std::string>();
-    app().getResolver()->resolve(app().getCustomConfig()["server-host"].asString(),
-                                 (std::function<void(const std::vector<trantor::InetAddress>&)>) [&dnsAddr](const std::vector<trantor::InetAddress>& addresses) {
-                     for (const auto &address: addresses) {
-                         LOG_INFO << "DNS: " << address.toIp();
-                         dnsAddr.push_back(address.toIp());
-                     }
-    });
-
-
-//    for (auto &[header, value]: req->headers()) {
-//        LOG_INFO << header << ": " << value;
-//    }
-
-    // block thread while async dns resolve is in progress
-    // with maximum total timeout of around 1s
-    size_t iterations = 0;
-    while (dnsAddr.empty() && 5 * iterations < 1000) {
-        iterations++;
-        std::this_thread::sleep_for(std::chrono::milliseconds(5));
-    }
-
-    for (const auto &entry: dnsAddr)
-        LOG_INFO << "DNS: " << entry;
     LOG_INFO << "Header: " << req->getHeader("x-forwarded-for");
 
     std::string realIp = req->getHeader("x-forwarded-for");
+    std::vector<std::string> dnsIps;
+    lookup(app().getCustomConfig()["server-host"].asString(), AF_INET, dnsIps);
 
-    if ((dnsAddr | std::views::filter([&](auto& x){ return x == realIp; })).empty())
+    if ((dnsIps | std::views::filter([&](auto& ip){return realIp == ip; })).empty())
     {
         //Check failed
         LOG_ERROR << "failed check!";
--- cpp/filters/api_MCServerFilter.h
+++ cpp/filters/api_MCServerFilter.h
@@ -9,11 +9,15 @@
 #include <drogon/HttpFilter.h>
 #include <drogon/HttpAppFramework.h>
 #include <ranges>
+#include <netdb.h>
+#include <vector>
 
 using namespace drogon;
 
 namespace api {
     class MCServerFilter : public HttpFilter<MCServerFilter> {
+    private:
+        int lookup(const std::string& host, int family, std::vector<std::string>& out) ;
     public:
         virtual void doFilter(const HttpRequestPtr &req,
                       FilterCallback &&fcb,
