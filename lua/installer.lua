dd605ade1f472134bae7602f67ed99d6e976b473
San32 <25187502+Sanduhr32@users.noreply.github.com>
2025-03-05 17:34:41 +0100

    write easy lua installer

---
+++ lua/installer.lua
@@ -0,0 +1,29 @@
+local comp = require("component")
+local sh = comp.shell
+local libraries = {
+    json = {
+        source = "https://github.com/Vurv78/qjson.lua/refs/heads/master",
+        files = {"qjson.lua"},
+        dir = "./qjson_lua/"
+    },
+    itself = {
+        source = "https://github.com/Sanduhr32/NewDashHorizons/refs/heads/master",
+        files = {"utils.lua", "env.lua", "me-status-lua"},
+        dir = "./"
+    }
+}
+
+for name, lib_data in pairs(libraries) do
+    print("| Installing " .. name)
+    print("| from: " .. lib_data.source)
+    local src = lib_data.source;
+    for i, filename in ipairs(lib_data.files) do
+        print("|- Downloading required file " .. filename)
+        local succ, fck = sh.execute("wget -Q \"" .. src .. "/" .. filename .. "\" \"" .. lib_data.dir .. filename .. "\"")
+        if not succ then
+            print(fck)
+            return
+        end
+    end
+    print("|============")
+end
