27a8a7a3001fa1055e345938f16c62789edf7161
M R S E <25187502+Sanduhr32@users.noreply.github.com>
2025-09-30 22:39:10 +0200

    Update naq_control.lua

--- lua/naq_control.lua
+++ lua/naq_control.lua
@@ -15,18 +15,20 @@ local monitor = thread.create(function()
     local min_percent = 0.1
     local max_percent = 0.98
     while true do
-        local data = {}
         local stored_power = parse(cap_sen[2])
         local total_power = parse(cap_sen[5])
         local avg5sout = cap_sen[10]
         local avg5sin = cap_sen[11]
         local drain = parse(cap_sen[7]) + parse(avg5sin:sub(1, avg5sin:find("(", 1, true) or -1))
         local prod = parse(avg5sout:sub(1, avg5sout:find("(", 1, true) or -1))
+        --[[
+        local data = {}
         data["eu"] = stored_power
         data["eu_max"] = total_power
         data["eu_in"] = prod
         data["eu_out"] = drain
-        --send(data, "Naquadah-Reaktor", "power")
+        send(data, "Naquadah-Reaktor", "power")
+        ]]--
 
         local state = naq_reac.isWorkAllowed()
         local nextstate = state
